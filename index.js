146227f691c4b4966cc163cd5a108632df918a60
neil patel <neil@myindianweddingapp.com>
2018-02-14 11:46:00 +0000

    Updated code & removed typo

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const HELP_MESSAGE = 'You can say tell me a fact about the valley, or you can sa
 const HELP_REPROMPT = 'What can I help you with?';
 const STOP_MESSAGE = 'Goodbye';
 
-const data = ['It is a common but incorrect myth that Silicon Valley is bro culture. Silicon Valley is geek culture'
-              ' A startup is a company designed to grow fast.'
-              'Venture investors want to back intellectually honest entreprenuers that can talk about strengths and weaknesses'
+const data = ['It is a common but incorrect myth that Silicon Valley is bro culture. Silicon Valley is geek culture',
+              ' A startup is a company designed to grow fast.',
+              'Venture investors want to back intellectually honest entreprenuers that can talk about strengths and weaknesses',
             ];
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.appId = APP_ID;
-    alexa.registerHanders(handlers);
+    alexa.registerHandlers(handlers);
     alexa.execute();
 };
 
